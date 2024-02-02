e66cb755de9491955508f8950b37558ced50e11c
Thalisson Goncalves da Silva <thalissongsilva70@gmail.com>
2024-02-02 00:00:08 -0300

    Atualiza css

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Title from './Title';
 import MissionCard from './MissionCard';
 import missions from '../data/missions';
+import '../App.css';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div>
         <Title headline="Missões" />
+        <div data-testid="missions" className="missions">
         { missions.map((mission) => (<MissionCard
           key={ mission.name }
           name={ mission.name }
@@ -15,6 +17,7 @@ class Missions extends React.Component {
           country={ mission.country }
           destination={ mission.destination }
         />))}
+        </div>
       </div>
     );
   }
